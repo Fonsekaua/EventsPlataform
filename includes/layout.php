848f2b104576097bb3e57df792cfed8775eefa3b
Fonsilva <fonsecakauasilva3@gmail.com>
2024-11-01 05:39:49 -0500

    atualizando a branch master

--- includes/layout.php
+++ includes/layout.php
@@ -1,13 +1,32 @@
+<?php 
+require "./actions/db.php";
+session_start();
+if(isset($_SESSION['usuario'])){
+    echo $_SESSION['usuario'];
+}
+?>
+
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>eventos</title>
+    <title>Document</title>
+    <link rel="stylesheet" href=<?= $globalCSS?>>
+    <link rel="stylesheet" href=<?= $css?>>
+
 </head>
 <body>
-    <footer>
-        <?php include "footer.php"?>
-    </footer>
+    <?php include "header.php";?>
+     <div id="box__info" class="">
+        por erros e falhas
+     </div>   
+    <main>
+        <?php include $content?>
+    </main>
+    <?php include "footer.php"?>
 </body>
+<script src=<?=$globalJS?>></script>
+<script src=<?=$js ?>></script>
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 </html>
--- views/Home.php
+++ views/Home.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página Inicial</title>
-</head>
-<body>
-    <?php require  "./includes/layout.php"?>
-</body>
-</html>
+<?php 
+$js = "";
+$css = "";
+
+$globalJS = "";
+$globalCSS = "";
+
+$content = "./views/pages/homePage.php";
+
+include "./includes/layout.php";
